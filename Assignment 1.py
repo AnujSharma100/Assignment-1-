f75c58d1b90a285b6799dc8ce6fdcb4cf379bc64
AnujSharma100 <117298925+AnujSharma100@users.noreply.github.com>
2022-11-19 19:31:40 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+#python program to get fibonacci series between 0 to 50
+# first_num,second_num=0,1
+# while second_num<50:
+#     print( second_num,end=',')
+    # first_num,second_num=second_num,first_num+second_num
+
+
+
+#python program to reverse a word entered by the user.
+#using negative indexing
+
+word=input("enter a word/string")
+reverse= word[::-1]
+print(reverse)
+
+
+
